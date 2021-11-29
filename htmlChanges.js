0b79391dd0d45a09e8a81d9926a80c586f6e3e2c
gabg4224 <gabg4224@gmail.com>
2021-11-29 15:22:08 -0300

    commit progress

---
+++ htmlChanges.js
@@ -0,0 +1,44 @@
+let academyChanges = [
+  `Somos una institución de formación artística que se
+    dedica a la educación de niños, jóvenes y adultos en el
+    área del baile y la danza, con el fin de brindarle a la
+    comunidad una oportunidad para conocer y aprender
+    los ritmos propios de la cultura afrocaribeña.`,
+
+  ` A partir de Marzo 2019 comienza sus actividades en
+Buenos Aires - Argentina, formando a bailadores y
+presentando una oportunidad única a los venezolanos
+en el exterior para disfrutar del calor del baile.`,
+  `Actualmente funcionamos de lunes a sábados con
+diferentes cursos activos de Salsa Casino y Bachata.
+Además de constantes talleres de baile
+complementarios como: Rueda de Casino,
+Desplazamiento en la Salsa en Pareja, Técnica de Giros,
+Estilo Chicas, Estilo Chicos, Afrocubano, etc.`,
+];
+
+let instructorsChanges = [
+  `Somos una institución de formación artística que se
+    dedica a la educación de niños, jóvenes y adultos en el
+    área del baile y la danza, con el fin de brindarle a la
+    comunidad una oportunidad para conocer y aprender
+    los ritmos propios de la cultura afrocaribeña.`,
+
+  ` A partir de Marzo 2019 comienza sus actividades en
+Buenos Aires - Argentina, formando a bailadores y
+presentando una oportunidad única a los venezolanos
+en el exterior para disfrutar del calor del baile.`,
+];
+
+let alumnsChanges = [
+  `Somos una institución de formación artística que se
+    dedica a la educación de niños, jóvenes y adultos en el
+    área del baile y la danza, con el fin de brindarle a la
+    comunidad una oportunidad para conocer y aprender
+    los ritmos propios de la cultura afrocaribeña.`,
+
+  ` A partir de Marzo 2019 comienza sus actividades en
+Buenos Aires - Argentina, formando a bailadores y
+presentando una oportunidad única a los venezolanos
+en el exterior para disfrutar del calor del baile.`,
+];
